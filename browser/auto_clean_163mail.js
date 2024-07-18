5b8edc407ba4269398af97694c954d834354739a
baojian.gu <baojian.gu@163.com>
2024-07-18 17:41:16 +0800

    出错时自动关闭错误提示框，然后再继续

--- browser/auto_clean_163mail.js
+++ browser/auto_clean_163mail.js
@@ -4,6 +4,17 @@
  */
 
 function autoDelete163Mail() {
+    var dialogs = document.getElementsByClassName("js-component-msgbox");
+    if (dialogs.length > 0) { //自动关闭错误提示
+         var buttons = dialogs[0].getElementsByClassName("nui-btn-text");
+         if (buttons.length > 0) {
+            buttons[0].click();
+            setTimeout(function(){ 
+                autoDelete163Mail();
+            }, 2000);
+            return;
+         }
+    }
     var headers = document.getElementsByClassName("frame-main-cont-head");
     var target = null;
     for(var i=0; i<headers.length; i++) {
@@ -45,6 +56,17 @@ setTimeout(function(){
  *  无人看时候10秒比较保险, 删除到最后时加载比较慢
  */
 function autoDelete163Mail() {
+    var dialogs = document.getElementsByClassName("js-component-msgbox");
+    if (dialogs.length > 0) {
+         var buttons = dialogs[0].getElementsByClassName("nui-btn-text");
+         if (buttons.length > 0) {
+            buttons[0].click();
+            setTimeout(function(){ 
+                autoDelete163Mail();
+            }, 2000);
+            return;
+         }
+    }
     var headers = document.getElementsByClassName("frame-main-cont-head");
     var target = null;
     for(var i=0; i<headers.length; i++) {
@@ -77,4 +99,57 @@ function autoDelete163Mail() {
 setTimeout(function(){ 
     autoDelete163Mail();
 }, 3000);
+
+
+/***
+ *  无人看管时，间隔长一些
+ * 打开对应的文件夹，然后点开开发者选项，运行脚本
+ *  无人看时候10秒比较保险, 删除到最后时加载比较慢
+ */
+function autoDelete163Mail() {
+    var dialogs = document.getElementsByClassName("js-component-msgbox");
+    if (dialogs.length > 0) { //自动关闭错误提示
+         var buttons = dialogs[0].getElementsByClassName("nui-btn-text");
+         if (buttons.length > 0) {
+            buttons[0].click();
+            setTimeout(function(){ 
+                autoDelete163Mail();
+            }, 2000);
+            return;
+         }
+    }
+
+    var headers = document.getElementsByClassName("frame-main-cont-head");
+    var target = null;
+    for(var i=0; i<headers.length; i++) {
+        if (headers[i].getAttribute("tabindex") == "0") {
+            target = headers[i]; //最后一个是顶部菜单
+        }
+        console.log(headers[i]);
+    }
+    
+    console.log(target);
+    
+    var buttons = target.getElementsByClassName("nui-toolbar-item");
+    
+    //选中邮件
+    console.log(buttons[0]);
+    buttons[0].getElementsByClassName("js-component-checkbox-dc")[0].click();
+    
+    console.log(buttons[1]);
+    
+    setTimeout(function() {
+        //点击删除按钮删除
+        buttons[1].getElementsByClassName("js-component-button")[0].click();
+        console.log("auto delete mail success");
+        setTimeout(function(){ 
+            autoDelete163Mail();
+        }, 8*1000); //等待8秒删除下一页， 无人看时候10秒比较保险, 删除到最后时加载比较慢
+    }, 500)
+}
+
+setTimeout(function(){ 
+    autoDelete163Mail();
+}, 3000);
+
 
