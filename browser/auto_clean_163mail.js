501a7ffd871dcda81e5f53a1c52c43740f65a80f
baojian.gu <gubaojian@163.com>
2024-08-12 14:50:01 +0800

    uWebSockets

--- browser/auto_clean_163mail.js
+++ browser/auto_clean_163mail.js
@@ -66,7 +66,7 @@ function autoDelete163Mail() {
         setTimeout(function(){ 
             autoDelete163Mail();
         }, 6500); //等待6.5秒删除下一页， 太短会弹出错误框， 无人看时候10秒比较保险
-    }, 500)
+    }, 800)
 }
 
 setTimeout(function(){ 
@@ -138,7 +138,7 @@ function autoDelete163Mail() {
         setTimeout(function(){ 
             autoDelete163Mail();
         }, 10*1000); //等待10秒删除下一页， 无人看时候10秒比较保险, 删除到最后时加载比较慢
-    }, 500)
+    }, 800)
 }
 
 setTimeout(function(){ 
@@ -212,7 +212,7 @@ function autoDelete163Mail() {
         setTimeout(function(){ 
             autoDelete163Mail();
         }, 8*1000); //等待8秒删除下一页， 无人看时候10秒比较保险, 删除到最后时加载比较慢
-    }, 500)
+    }, 800)
 }
 
 setTimeout(function(){ 
@@ -221,3 +221,75 @@ setTimeout(function(){
 
 
 // document.getElementsByClassName("nui-tabs-item-selected").length
+
+
+
+
+
+var wakeLock = null; 
+function lockScreen() {
+    if (navigator.wakeLock == null) {
+        console.error('Your browser is not support WakeLock API!');
+        return;
+    }
+    if (wakeLock) {
+        return;
+    }
+    navigator.wakeLock.request('screen').then(result => {
+        wakeLock = result;
+        console.log('Wake Lock is actived!');
+        wakeLock.addEventListener('release', () => {
+            wakeLock = null;
+            console.log('Wake Lock is released!');
+        });
+    }).catch((err) => {
+        console.error(`Wake Lock is faild：${err.message}`);
+    });
+};
+
+function autoDelete163Mail() {
+    lockScreen();
+    var dialogs = document.getElementsByClassName("js-component-msgbox");
+    if (dialogs.length > 0) { //自动关闭错误提示
+         var buttons = dialogs[0].getElementsByClassName("nui-btn-text");
+         if (buttons.length > 0) {
+            buttons[0].click();
+            setTimeout(function(){ 
+                autoDelete163Mail();
+            }, 2000);
+            return;
+         }
+    }
+
+    var headers = document.getElementsByClassName("frame-main-cont-head");
+    var target = null;
+    for(var i=0; i<headers.length; i++) {
+        if (headers[i].getAttribute("tabindex") == "0") {
+            target = headers[i]; //最后一个是顶部菜单
+        }
+        console.log(headers[i]);
+    }
+    
+    console.log(target);
+    
+    var buttons = target.getElementsByClassName("nui-toolbar-item");
+    
+    //选中邮件
+    console.log(buttons[0]);
+    buttons[0].getElementsByClassName("js-component-checkbox-dc")[0].click();
+    
+    console.log(buttons[1]);
+    
+    setTimeout(function() {
+        //点击删除按钮删除
+        buttons[1].getElementsByClassName("js-component-button")[0].click();
+        console.log("auto delete mail success");
+        setTimeout(function(){ 
+            autoDelete163Mail();
+        }, 7*1000); //等待8秒删除下一页， 无人看时候10秒比较保险, 删除到最后时加载比较慢
+    }, 800)
+}
+
+setTimeout(function(){ 
+    autoDelete163Mail();
+}, 3000);
