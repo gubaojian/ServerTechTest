ec31ad5a23e16e53522b9310aa2cd57d31f45473
baojian.gu <gubaojian@163.com>
2025-05-04 20:45:05 +0800

    增加任务测试

--- wss/websocketppserver/websocketpp/websocket_router_no_tts_test.cc
+++ wss/websocketppserver/websocketpp/websocket_router_no_tts_test.cc
@@ -18,90 +18,30 @@
 #include <boost/asio.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
-#include "websocketpp/config/asio_no_tls_client.hpp"
+#include <websocketpp/config/asio_client.hpp>
 #include "websocketpp/client.hpp"
 
-typedef websocketpp::client<websocketpp::config::asio_client> client;
 
-using websocketpp::lib::placeholders::_1;
-using websocketpp::lib::placeholders::_2;
-using websocketpp::lib::bind;
 
-// pull out the type of messages sent by our config
+typedef websocketpp::client<websocketpp::config::asio_client> plain_client;
+typedef websocketpp::client<websocketpp::config::asio_tls_client> tls_client;
 typedef websocketpp::config::asio_client::message_type::ptr message_ptr;
 
+struct plain_connection{
+    plain_client::connection_ptr conn = nullptr;
+    int64_t receiveMessageCount = 0;
+    int64_t sendMessageCount = 0;
+};
 
-static auto receive_start = std::chrono::high_resolution_clock::now();
+struct tls_connection{
+    tls_client::connection_ptr conn = nullptr;
+    int64_t receiveMessageCount = 0;
+    int64_t sendMessageCount = 0;
+};
 
-
-static int64_t receive_count = 0;
-
-
-// This message handler will be invoked once for each incoming message. It
-// prints the message and then sends a copy of the message back to the server.
-static void on_message(client* c, websocketpp::connection_hdl hdl, message_ptr msg) {
-    if (msg->get_payload().starts_with("start_")) {
-        receive_start = std::chrono::high_resolution_clock::now();
-        std::cout << "receive message start " << std::endl;
-    }
-    receive_count ++;
-
-    if (msg->get_payload().starts_with("end_") || receive_count % 10000 == 0) {
-        auto end = std::chrono::high_resolution_clock::now();
-        auto used = std::chrono::duration_cast<std::chrono::milliseconds>(end - receive_start);
-        std::cout << "receive message " << used.count() << "ms " << receive_count  << std::endl;
-    }
-}
-
-
-/**
- 128
- start new turn total message 2000002
- receive used 8686.95ms
- 
- 1024
- 
- start new turn total message 4000002
- receive used 11824.5ms
- send message speed 3906.25 mb
- 400-500mb/s
- 
- 
- 4096
- start new turn total message 2000002
- receive used 6941.19ms
- send message speed 7812 mb
- 
- 1.13gb / s
- 
- start new turn total message 4000002
- receive used 19493.1ms
- send message speed  15625 mb
- 0.78 gb/s
- 
- 
- 4096*2
- start new turn total message 2000002
- receive used 9383.77ms
- send message speed 15625 mb
- 1.61gb/s
- 
- 
- 4096*4
- start new turn total message 2000002
- receive used 32324.3ms
- send message speed 31250 mb
- 
- 1gb/s
- 
-  */
-
-static client* mainClient;
-websocketpp::lib::shared_ptr<boost::asio::ssl::context> ctx;
-
-void connect(client* client, const std::string uri) {
+void connect_plain(plain_client* client, const std::string uri) {
     websocketpp::lib::error_code ec;
-    client::connection_ptr con =  client->get_connection(uri, ec);
+    plain_client::connection_ptr con =  client->get_connection(uri, ec);
     if (ec) {
         std::cout << "could not create connection because: " << ec.message() << std::endl;
         return;
@@ -114,43 +54,109 @@ void connect(client* client, const std::string uri) {
     con->set_fail_handler([=](websocketpp::connection_hdl hdl) {
         std::cout << "client failed connect " << &hdl << std::endl;
         std::cout << "client failed connect try reconnect " << std::endl;
-        
-        boost::asio::steady_timer timer(mainClient->get_io_service());
-        timer.expires_after(std::chrono::seconds(3));
-        timer.async_wait([=] (const boost::system::error_code& error_code){
-            std::cout << "client failed connect reconnect " << std::endl;
-            connect(client, uri);
+        //https://stackoverflow.com/questions/76819327/asio-async-wait-gives-operation-canceled
+        std::shared_ptr<boost::asio::steady_timer> timer = std::make_shared<boost::asio::steady_timer>(client->get_io_service());
+        timer->expires_after(std::chrono::seconds(30 + std::rand()%30));
+        timer->async_wait([timer, client, uri] (const boost::system::error_code& error_code){
+            if (error_code) {
+                std::cout << "error code " << error_code.message()
+                << " " << " " << error_code.location() << std::endl;
+                return ;
+            }
+            std::cout << "client failed connect reconnect " << error_code << std::endl;
+            connect_plain(client, uri);
             std::cout << "client failed connect connect again " << std::endl;
             
         });
+        con->set_fail_handler(nullptr);
     });
     con->set_close_handler([=](websocketpp::connection_hdl hdl){
         std::cout << "client be closed " << &hdl << std::endl;
         //timer
-        boost::asio::steady_timer timer(mainClient->get_io_service());
-        timer.expires_after(std::chrono::seconds(3));
-        timer.async_wait([=] (const boost::system::error_code& error_code){
-            std::cout << "client failed connect reconnect " << std::endl;
-            connect(mainClient, uri);
+        std::shared_ptr<boost::asio::steady_timer> timer = std::make_shared<boost::asio::steady_timer>(client->get_io_service());
+        timer->expires_after(std::chrono::seconds(30 + std::rand()%30));
+        timer->async_wait([timer, client, uri] (const boost::system::error_code& error_code){
+            if (error_code) {
+                std::cout << "error code " << error_code.message()
+                << " " << " " << error_code.location() << std::endl;
+                return ;
+            }
+            std::cout << "client failed connect reconnect " << error_code << std::endl;
+            connect_plain(client, uri);
             std::cout << "client failed connect connect again " << std::endl;
             
         });
+        con->set_close_handler(nullptr);
     });
     con->set_message_handler([](websocketpp::connection_hdl hdl, message_ptr msg){
         std::cout << "on message client" << &hdl << " " << msg->get_payload() << std::endl;
         
     });
-    client::connection_ptr mainConn = client->connect(con);
+   plain_client::connection_ptr mainConn = client->connect(con);
+  
+    mainConn = nullptr;
    
 }
 
-int websocket_router_no_tts_test_main(int argc, const char * argv[]) {
+void connect_tls(const tls_client* client, const std::string uri) {
+    
+}
+
+void runWSRouter() {
     auto start = std::chrono::high_resolution_clock::now();
     auto end = std::chrono::high_resolution_clock::now();
     auto used = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
     
     
-    client client;
+    plain_client client;
+    std::string uri = "ws://127.0.0.1:9001/wsg?role=server&serverAppId=081713074824&serverAppToken=Y8FG0tbs7pgujQccNcEABIuW1it2Rhfw";
+
+    try {
+        // Set logging to be pretty verbose (everything except message payloads)
+        client.set_access_channels(websocketpp::log::alevel::none);
+        client.set_error_channels(websocketpp::log::elevel::none);
+        
+        // Initialize ASIO
+        client.init_asio();
+        // Register our message handler
+        connect_plain(&client, uri);
+        connect_plain(&client, uri);
+       //boost::asio::steady_timer timer(client.get_io_service());
+       //timer.expires_after(std::chrono::seconds(1));
+       //timer.async_wait(send_message_bench);
+
+        // Start the ASIO io_service run loop
+        // this will cause a single connection to be made to the server. c.run()
+        // will exit when this connection is closed.
+        plain_client* ptr = &client;
+        std::thread background([ptr] {
+            auto start = std::chrono::high_resolution_clock::now();
+            for(int i=0; i<10000*200; i++) {
+                boost::asio::post(ptr->get_io_service(), [] {
+                    //std::cout << "post execute command";
+                });
+            }
+            auto end = std::chrono::high_resolution_clock::now();
+            auto used = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+            std::cout << "post message done " << used.count() << "ms "  << std::endl;
+           
+        });
+        
+        client.run();
+    } catch (websocketpp::exception const & e) {
+        std::cout << "router connect websocket exception " << e.what() << std::endl;
+    }
+}
+
+void runWSSRouter() {
+    auto start = std::chrono::high_resolution_clock::now();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto used = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    
+    
+    tls_client client;
+    websocketpp::lib::shared_ptr<boost::asio::ssl::context> ctx;
+   
     std::string uri = "ws://127.0.0.1:9001/wsg?role=server&serverAppId=081713074824&serverAppToken=Y8FG0tbs7pgujQccNcEABIuW1it2Rhfw";
 
     try {
@@ -164,14 +170,13 @@ int websocket_router_no_tts_test_main(int argc, const char * argv[]) {
         client.init_asio();
         
         ctx = websocketpp::lib::make_shared<boost::asio::ssl::context>(boost::asio::ssl::context::tlsv13_client);
-         client.set_tls_init_handler([](websocketpp::connection_hdl hdl) {
+         client.set_tls_init_handler([=](websocketpp::connection_hdl hdl) {
              return ctx;
           });
 
         // Register our message handler
-        connect(&client, uri);
-        connect(&client, uri);
-        mainClient = &client;
+        connect_tls(&client, uri);
+        connect_tls(&client, uri);
        //boost::asio::steady_timer timer(client.get_io_service());
        //timer.expires_after(std::chrono::seconds(1));
        //timer.async_wait(send_message_bench);
@@ -184,7 +189,10 @@ int websocket_router_no_tts_test_main(int argc, const char * argv[]) {
     } catch (websocketpp::exception const & e) {
         std::cout << "router connect websocket exception " << e.what() << std::endl;
     }
-    
+}
+
+int websocket_router_no_tts_test_main(int argc, const char * argv[]) {
+    runWSRouter();
     
     return 0;
 }
