9ad4982750915c0663c3e769866ac3e41a80b334
baojian.gu <gubaojian@163.com>
2025-06-03 11:14:30 +0800

    测试源代码

--- jwt/cplus/BacktraceTest/BacktraceTest/main.cpp
+++ jwt/cplus/BacktraceTest/BacktraceTest/main.cpp
@@ -28,9 +28,26 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     backward::SignalHandling handle;
+    
+    backward::StackTrace st; st.load_here(32);
+
+    backward::TraceResolver tr;
+    tr.load_stacktrace(st);
+    for (size_t i = 0; i < st.size(); ++i) {
+        backward::ResolvedTrace trace = tr.resolve(st[i]);
+        std::cout << "#" << i
+            << " " << trace.object_filename
+            << " " << trace.object_function
+            << " [" << trace.addr << "]"
+            << " [" << trace.source.filename << "]"
+            << " [" << trace.source.line << "]"
+        << std::endl;
+    }
+    
     std::thread th([]{
         test();
     });
     th.join();
+    
     return 0;
 }
