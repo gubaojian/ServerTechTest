c970f805dbf40c0507b9fafdc956a38caed59502
baojian.gu <gubaojian@163.com>
2025-06-04 11:17:17 +0800

    rnacos

--- jwt/cplus/MemoryTest/MemoryTest/main.cpp
+++ jwt/cplus/MemoryTest/MemoryTest/main.cpp
@@ -13,10 +13,19 @@
 #include <list>
 
 /**
+ *  https://github.com/KDE/kcachegrind
+ *    https://user-images.githubusercontent.com/19547/88445602-879c0300-cdd8-11ea-92cc-ba22b1c5d121.png
+ *
+ *    https://github.com/mruby/mruby/issues/5047
+ *
+ *  https://github.com/mruby/mruby/pull/5069/files
+ *    https://valgrind.org/docs/manual/manual.html
+ *
  * https://www.ctyun.cn/developer/article/444952717525061
  *  https://github.com/openjdk/jdk21/blob/jdk-21%2B35/src/hotspot/os/linux/os_linux.cpp#L5518
- *  https://github.com/alibaba/arthas/issues/3020 
+ *  https://github.com/alibaba/arthas/issues/3020
  * std::list std::deque 调用shrink_to_fit试试。
+ *  https://www.cnblogs.com/shizioo/p/rnacos_readme.html
  * 尝试替代queue保持各平台内存一致性。或者创建新对象替换老对象的方式释放。
  * std::queue 在linux平台上不释放内存，具体看deque实现源代码
  *  https://github.com/gcc-mirror/gcc/blob/master/libstdc%2B%2B-v3/include/bits/stl_deque.h
