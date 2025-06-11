58451e5beb89dcc0a9370a5a22b052494d8a77ae
gubaojian <gubaojian@163.com>
2025-06-11 15:36:04 +0800

    batch message post

--- wss/websocketpp3/websocketpp/batch_queue_test.cc
+++ wss/websocketpp3/websocketpp/batch_queue_test.cc
@@ -15,6 +15,7 @@ int batch_queue_test(int argc, const char * argv[]){
     boost::asio::io_context encode_send_frame_io_context;
     boost::asio::io_context& io_context = encode_send_frame_io_context;
     auto worker_guard = boost::asio::make_work_guard(io_context);
+ 
     
     std::thread encodeSendFrameThread([&io_context] {
         io_context.run();
@@ -29,10 +30,18 @@ int batch_queue_test(int argc, const char * argv[]){
         });
     });
     
-    for(int i=0; i<10000*100; i++) {
+    auto start = std::chrono::high_resolution_clock::now();
+    auto end = std::chrono::high_resolution_clock::now();
+    auto used = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    start = std::chrono::high_resolution_clock::now();
+    for(int i=0; i<1000*10*400; i++) {
         batchPost.add(std::make_shared<std::string>("hello world"));
     }
     batchPost.flush();
+    end = std::chrono::high_resolution_clock::now();
+    
+    used = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "batch message used " << used << std::endl;
     
     encodeSendFrameThread.join();
     
