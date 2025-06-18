491899f0415545be01aa841400e43ca4b3e6faac
baojian.gu <gubaojian@163.com>
2025-06-18 20:08:36 +0800

    save code

---
+++ jwt/cplus/io/StringPoolTest/StringPoolTest/BatchPostMessage.hpp
@@ -0,0 +1,59 @@
+#ifndef BatchPostMessage_hpp
+#define BatchPostMessage_hpp
+
+#include <vector>
+#include <memory>
+#include <iostream>
+
+
+template <typename T>
+using OnBatchMessaage = std::function<void(std::shared_ptr<std::vector<T>>&)>;
+
+
+
+template <typename T>
+class BatchPostMessage {
+    public :
+    explicit BatchPostMessage(size_t size)
+     : batchSize(size)
+     , messages(std::make_shared<std::vector<T>>(size))
+    {
+        
+    };
+    
+public:
+    void add(const T& t) {
+        messages->push_back(t);
+        flushIfOverBatchLimit();
+    }
+    
+    void flush() {
+        if (messages->size() > 0) {
+            if (_onBatchMessaage != nullptr) {
+                _onBatchMessaage(messages);
+            }
+            messages = std::make_shared<std::vector<T>>();
+        }
+    }
+    
+    void setOnBatchMessaage(OnBatchMessaage<T> action) {
+        _onBatchMessaage = action;
+    }
+    
+private:
+    void flushIfOverBatchLimit() {
+        if (messages->size() >= batchSize) {
+            if (_onBatchMessaage != nullptr) {
+                _onBatchMessaage(messages);
+            }
+            messages = std::make_shared<std::vector<T>>();
+        }
+    }
+    
+private:
+    size_t batchSize = 128;
+    OnBatchMessaage<T> _onBatchMessaage = nullptr;
+    std::shared_ptr<std::vector<T>> messages;
+};
+
+#endif /* BatchPost_hpp */
