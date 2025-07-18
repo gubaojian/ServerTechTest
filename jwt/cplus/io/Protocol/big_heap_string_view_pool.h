99e2d198fed855175e9a3f2e7e4cc790717be73a
gubaojian <gubaojian@163.com>
2025-07-18 08:41:35 +0800

    增加一个atomic版本，留着后续优化

--- jwt/cplus/io/Protocol/big_heap_string_view_pool.h
+++ jwt/cplus/io/Protocol/big_heap_string_view_pool.h
@@ -90,6 +90,78 @@ private:
     int64_t poolSize = 0;
 };
 
+class AtomicBigHeapStringViewPool {
+public:
+    explicit  AtomicBigHeapStringViewPool(size_t size) {
+        buffer = new char[size + 8*1024]; //add padding to avoid check array bounds
+        this->poolSize = size;
+        allocOffset = 0;
+        returnOffset = poolSize;
+    }
+    ~ AtomicBigHeapStringViewPool() {
+        if (buffer != nullptr) {
+            delete[] buffer;
+            buffer = nullptr;
+        }
+    }
+public:
+    std::string_view allocateStringViewInPool(const std::string& message) {
+        return allocateStringViewInPool(message.data(), message.size());
+    }
+
+    std::string_view allocateStringViewInPool(const std::string_view& message) {
+        return allocateStringViewInPool(message.data(), message.size());
+    }
+
+    std::string_view allocateStringViewInPool(const char* data, size_t length) {
+        if (length > 4*1024) { //for bug message just alloc directly
+            return std::string_view{};
+        }
+        totalGet++;
+        int64_t remainSize = returnOffset - allocOffset;
+        if (remainSize > length) {
+            int64_t offset = allocOffset%poolSize;
+            char* from = buffer + offset;
+            memcpy(from, data, length);
+            allocOffset += length;
+            cacheHit++;
+            return std::string_view{from, length};
+        }
+        return std::string_view{};
+    }
+
+    void deallocateStringViewInPool(const std::string_view& message) {
+        if (!message.empty()) {
+            //加入debug选项。
+            if (message.data() < buffer || message.data() >= (buffer + poolSize + 8*1024)) {
+                std::cout << "deallocateStringViewInPool call release message not in pool" << std::endl;
+                return;
+            }
+            returnOffset += message.size();
+        }
+    }
+
+    int64_t remainSpace() const {
+        return returnOffset - allocOffset;
+    }
+
+    int64_t getTotalGet() const {
+        return totalGet;
+    }
+
+    int64_t getCacheHit() const {
+        return cacheHit;
+    }
+
+private:
+    char* buffer = nullptr;
+    int64_t allocOffset;
+    int64_t returnOffset; //none need atomic, later see update is acceptable
+    int64_t totalGet = 0;
+    int64_t cacheHit = 0;
+    int64_t poolSize = 0;
+};
+
 
 
 
