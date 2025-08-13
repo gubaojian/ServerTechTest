198a1275b905aba0b25e9f64f26fbf253456214c
baojian.gu <gubaojian@163.com>
2025-08-13 23:05:25 +0800

    增加rsa/pss 签名验证

--- jwt/jwtTest2/src/main/java/org/jwt/RsaUtil.java
+++ jwt/jwtTest2/src/main/java/org/jwt/RsaUtil.java
@@ -4,7 +4,9 @@ package org.jwt;
 import javax.crypto.Cipher;
 import java.nio.charset.StandardCharsets;
 import java.security.*;
+import java.security.spec.MGF1ParameterSpec;
 import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.PSSParameterSpec;
 import java.security.spec.X509EncodedKeySpec;
 import java.util.Base64;
 
@@ -81,6 +83,51 @@ public class RsaUtil {
         byte[] decodedBytes = Base64.getDecoder().decode(encryptedText);
         byte[] decryptedBytes = cipher.doFinal(decodedBytes);
         return new String(decryptedBytes, StandardCharsets.UTF_8);
+    }
+
+    public static String sign(String data, String privateKeyStr) throws Exception {
+        byte[] privateKeyBytes = Base64.getDecoder().decode(privateKeyStr);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(privateKeyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+        PrivateKey privateKey = keyFactory.generatePrivate(keySpec);
+
+        Signature signature = Signature.getInstance("RSASSA-PSS");
+        PSSParameterSpec pssParams = new PSSParameterSpec(
+                "SHA-256",
+                "MGF1",
+                MGF1ParameterSpec.SHA256,
+                32,  // 盐长度
+                1    //  trailers field
+        );
+
+        signature.initSign(privateKey);
+        signature.setParameter(pssParams);
+
+        signature.update(data.getBytes(StandardCharsets.UTF_8));
+        byte[] sign = signature.sign();
+        return Base64.getEncoder().encodeToString(sign);
+    }
+
+    public static boolean verify(String sign, String plainText, String publicKeyStr) throws Exception {
+        byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyStr);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
+        KeyFactory keyFactory = KeyFactory.getInstance(algorithm);
+        PublicKey publicKey = keyFactory.generatePublic(keySpec);
+        Signature signature = Signature.getInstance("RSASSA-PSS");
+        PSSParameterSpec pssParams = new PSSParameterSpec(
+                "SHA-256",
+                "MGF1",
+                MGF1ParameterSpec.SHA256,
+                32,  // 盐长度
+                1    //  trailers field
+        );
+
+        signature.initVerify(publicKey);
+        signature.setParameter(pssParams);
+
+        signature.update(plainText.getBytes(StandardCharsets.UTF_8));
+
+       return signature.verify(Base64.getDecoder().decode(sign.getBytes(StandardCharsets.UTF_8)));
     }
 
 
